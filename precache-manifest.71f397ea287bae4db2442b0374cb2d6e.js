f153d5943bf01151beb43419784566e2b9758b15
unknown <romka.fominyx@mail.ru>
2020-10-21 19:04:53 +0400

    Updates

---
+++ precache-manifest.71f397ea287bae4db2442b0374cb2d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa88792c6f38ec734dcc581caa98a3d",
+    "url": "/myReactTest/index.html"
+  },
+  {
+    "revision": "b1b1cbaa70365f35e453",
+    "url": "/myReactTest/static/css/main.8025f6b2.chunk.css"
+  },
+  {
+    "revision": "2c870983cbd638b56be7",
+    "url": "/myReactTest/static/js/2.6c2d92ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myReactTest/static/js/2.6c2d92ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b1cbaa70365f35e453",
+    "url": "/myReactTest/static/js/main.60217321.chunk.js"
+  },
+  {
+    "revision": "933d0e827828965d911b",
+    "url": "/myReactTest/static/js/runtime-main.e2917707.js"
+  }
+]);
